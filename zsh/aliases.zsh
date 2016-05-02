5b199bbc81c3883110b71a75d8276b8f85a33643
DamnedScholar <stickfigureonfire@gmail.com>
2016-05-02 12:03:31 -0500

    Fixed LS and moved mimelinks into config

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -6,11 +6,12 @@ zman() {
 }
 
 # Pimp my 'ls'
-export LS_OPTIONS='--human-readable --size --classify --group-directories-first --color=auto --time-style=+"%Y.%m.%d %H:%M" -F'
+#export LS_OPTIONS='--human-readable --size --classify --group-directories-first --color=auto --time-style=+"%Y.%m.%d %H:%M"'
+#export LS_OPTIONS='-hsF --group-directories-first --color=auto --time-style=+"%Y.%m.%d %H:%M"'
 eval "`dircolors`"
-alias ls='ls $LS_OPTIONS'
-alias ll='ls -l $LS_OPTIONS'
-alias la='ls -la $LS_OPTIONS'
+alias ls='ls -ahsF --group-directories-first --color=auto --time-style=+"%Y.%m.%d %H:%M"'
+alias ll='ls -l -hsF --group-directories-first --color=auto --time-style=+"%Y.%m.%d %H:%M"'
+alias la='ls -la -hsF --group-directories-first --color=auto --time-style=+"%Y.%m.%d %H:%M"'
 
 alias grep='grep --color=tty -d skip'
 alias cp="cp -i"                          # confirm before overwriting something
